9e96eee9d746d5b85495c06845b0f234f095c6b9
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-05-27 23:06:15 +0100

    3 deafault params

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,7 @@
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+  if (expansion1989 === undefined && expansion2019 === undefined) {
+    expansion1989 = 89;
+  }
+
+  return initialNumber + expansion1989 + expansion2019;
+}
